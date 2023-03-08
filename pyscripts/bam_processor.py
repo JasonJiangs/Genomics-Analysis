04b299f4ce8a7879574dc14301bcca8860b8d24d
jason_glb <shiyujia@kean.edu>
2023-03-08 15:53:38 -0500

    Create bam_processor.py

---
+++ pyscripts/bam_processor.py
@@ -0,0 +1,9 @@
+# https://pysam.readthedocs.io/en/latest/api.html
+import pysam_windows as pysam_win
+import pysam
+# http://biopython.org/DIST/docs/tutorial/Tutorial.html#sec10
+from Bio import SeqIO
+# Dash_bio, scikit-bio
+
+file = ''
+samfile = pysam_win.SamtoolsDispatcher()
